36aab4ef3a7a57f2bd7ce820f5e19f9d4e2c68b0
Phillippe Santana <phillippelevidad@gmail.com>
2022-01-24 18:25:15 -0300

    Added API

---
+++ src/update.js
@@ -0,0 +1,27 @@
+import handler from "./util/handler";
+import dynamoDb from "./util/dynamoDb";
+
+export const main = handler(async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      userId: "123",
+      noteId: event.pathParameters.id,
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET content = :content, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":attachment": data.attachment || null,
+      ":content": data.content || null,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW",
+  };
+
+  await dynamoDb.update(params);
+  return { status: true };
+});
